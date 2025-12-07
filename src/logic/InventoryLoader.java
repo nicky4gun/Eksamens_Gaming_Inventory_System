f32e61bacbc0a93875908c51a0a3610583df2d56
nicky4gun <nicklas.stagesorensen@outlook.dk>
2025-12-07 11:28:19 +0100

    update 4th

---
+++ src/logic/InventoryLoader.java
@@ -0,0 +1,12 @@
+package logic;
+
+
+import models.Player;
+
+public class InventoryLoader {
+    public void ShowPlayerInventory(int playerID){
+
+        String sql
+
+    }
+}
--- src/models/Weapon.java
+++ src/models/Weapon.java
@@ -1,17 +1,20 @@
 package models;
 
 import models.enums.ItemCategory;
+import models.enums.WeaponCategory;
 
 public class Weapon extends Item {
     private int damage;
     private double attackSpeed;
     private boolean isOneHanded;
+    private WeaponCategory weaponCategory;
 
     public Weapon(String name, double weight, int damage, double attackSpeed, boolean isOneHanded, ItemCategory category) {
         super(name, weight, category);
         this.damage = damage;
         this.attackSpeed = attackSpeed;
         this.isOneHanded = isOneHanded;
+        this.weaponCategory = weaponCategory;
     }
 
     public int getDamage() {
@@ -23,6 +26,8 @@ public class Weapon extends Item {
     public boolean getIsOneHanded() {
         return isOneHanded;
     }
+    public WeaponCategory weaponCategory(){return weaponCategory;}
+
 
     @Override
     public String getItemType() {
@@ -36,6 +41,7 @@ public class Weapon extends Item {
                 "damage=" + damage +
                 ", attackSpeed=" + attackSpeed +
                 ", isOneHand=" + isOneHanded +
+                ", weaponCategory= " + weaponCategory +
                 '}';
     }
 }
